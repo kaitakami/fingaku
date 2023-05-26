d0a18543a8944cddb219549d0b938fea35dcb60a
Kai Takami <kaitakami@protonmail.com>
2023-05-25 21:50:01 -0600

    feat: create landing page with floatui components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,25 @@
+import Layout from "@/components/Layout";
+import CTA from "@/components/ui/CTA";
+import FAQs from "@/components/ui/FAQs";
+import Features from "@/components/ui/Features";
+import Hero from "@/components/ui/Hero";
+import Pricing from "@/components/ui/Pricing";
+import Testimonial from "@/components/ui/Testimonial";
+import VisualFeatures from "@/components/ui/VisualFeatures";
 import { type NextPage } from "next";
-import Head from "next/head";
 
 const Home: NextPage = () => {
 
   return (
-    <>
-      <Head>
-        <title>Fingaku</title>
-        <meta name="description" content="Maneja tus finanzas como estudiante" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        Hola Fingaku
-      </main>
-    </>
+    <Layout>
+      <Hero />
+      <VisualFeatures />
+      <Features />
+      <CTA />
+      <Testimonial />
+      <Pricing />
+      <FAQs />
+    </Layout>
   );
 };
 
