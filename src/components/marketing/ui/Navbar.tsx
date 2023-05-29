4ebd0e8a6d67bc5599f8584a7bc343c961e97dec
Kai Takami <kaitakami@protonmail.com>
2023-05-29 14:06:34 -0600

    feat: show dashboard link if user is authed

--- src/components/marketing/ui/Navbar.tsx
+++ src/components/marketing/ui/Navbar.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 import NavHeader from './NavHeader'
 import NavLink from './NavLink'
+import { useSession } from 'next-auth/react'
 
 const Navbar = () => {
 
     const [state, setState] = useState(false)
     const menuBtnEl = useRef<HTMLButtonElement>(null)
+    const { status: sessionStatus } = useSession()
 
     const navigation = [
         { name: "Características", href: "/#caracteristicas" },
@@ -47,15 +49,19 @@ const Navbar = () => {
                             }
                         </ul>
                         <div className="gap-x-6 items-center justify-end mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
-                            <Link href="/signin" className="block hover:text-zinc-800">
-                                Inicia sesión
+                            {sessionStatus === "authenticated" ? <Link href="/dashboard" className="block hover:text-zinc-800">
+                                Dashboard
                             </Link>
-                            <NavLink href="/pricing" className="flex items-center justify-center gap-x-1 text-sm text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
-                                Comienza ahora
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                                    <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                                </svg>
-                            </NavLink>
+                                : <>
+                                    <Link href="/signin" className="block hover:text-zinc-800">
+                                        Inicia sesión
+                                    </Link>
+                                    <NavLink href="/pricing" className="flex items-center justify-center gap-x-1 text-sm text-white font-medium custom-btn-bg border border-gray-500 active:bg-gray-900 md:inline-flex">
+                                        Comienza ahora
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                                            <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
+                                        </svg>
+                                    </NavLink></>}
                         </div>
                     </div>
                 </div>
