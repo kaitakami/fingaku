f92831cb76718216d7da30cabb6db2cf25f29df7
Kai Takami <kaitakami@protonmail.com>
2023-05-28 21:48:19 -0600

    feat: create app responsive navbar

--- src/components/app/ui/Navbar.tsx
+++ src/components/app/ui/Navbar.tsx
@@ -1,16 +1,24 @@
-import { useEffect, useRef, useState } from "react"
+"use client"
 
-// Avtar with darpdown menu
-const AvatarMenue = () => {
+import { useEffect, useRef, useState } from "react"
+import Link from "next/link"
+import Brand from "@/components/marketing/ui/Brand"
+import { signOut, useSession } from "next-auth/react"
+import { ApplicationError } from "@/lib/errors"
+import { useRouter } from "next/router"
+import Image from "next/image"
+
+const AvatarMenu = () => {
 
   const [state, setState] = useState(false)
   const profileRef = useRef<HTMLButtonElement>(null)
+  const { data: session } = useSession()
 
   const navigation = [
     { title: "Dashboard", path: "/dashboard" },
-    { title: "Objetivos", path: "/objetivos" },
-    { title: "Gastos", path: "/gastos" },
-    { title: "Presupuestos", path: "/presupuestos" },
+    { title: "Objetivos", path: "/dashboard/objetivos" },
+    { title: "Gastos", path: "/dashboard/gastos" },
+    { title: "Presupuestos", path: "/dashboard/presupuestos" },
   ]
 
 
@@ -26,27 +34,31 @@ const AvatarMenue = () => {
   return (
     <div className="relative border-t lg:border-none">
       <div className="">
-        <button ref={profileRef} className="hidden w-10 h-10 outline-none rounded-full ring-offset-2 ring-gray-200 lg:focus:ring-2 lg:block"
+        <button ref={profileRef} className="hidden outline-none rounded-full ring-offset-2 ring-gray-200 lg:focus:ring-2 lg:block"
           onClick={() => setState(!state)}
         >
-          <img
-            src="https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg"
-            className="w-full h-full rounded-full"
+          <Image
+            src={session?.user.image || ""}
+            className="rounded-full"
+            alt={`${session?.user.name || ""} profile photo`}
+            width={40}
+            height={40}
+            unoptimized
           />
         </button>
       </div>
-      <ul className={`bg-white top-14 right-0 mt-6 space-y-6 lg:absolute lg:border lg:rounded-md lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${state ? '' : 'lg:hidden'}`}>
+      <ul className={`bg-white top-14 right-0 mt-6 space-y-6 lg:absolute z-10 lg:border lg:rounded-md lg:w-52 lg:shadow-md lg:space-y-0 lg:mt-0 ${state ? '' : 'lg:hidden'}`}>
         {
-          navigation.map((item, idx) => (
-            <li key={idx}>
+          navigation.map((item, id) => (
+            <li key={id}>
               <a className="block text-gray-600 hover:text-gray-900 lg:hover:bg-gray-50 lg:p-3" href={item.path}>
                 {item.title}
               </a>
             </li>
           ))
         }
-        <button className="block w-full text-justify text-gray-600 hover:text-gray-900 border-t py-3 lg:hover:bg-gray-50 lg:p-3">
-          Logout
+        <button className="block w-full text-justify text-gray-600 hover:text-gray-900 border-t py-3 lg:hover:bg-gray-50 lg:p-3" onClick={() => { signOut().catch((err: string) => new ApplicationError(err)) }}>
+          Salir de sesión
         </button>
       </ul>
     </div>
@@ -54,22 +66,19 @@ const AvatarMenue = () => {
 }
 
 const Navbar = () => {
-
   const [state, setState] = useState(false)
+  const router = useRouter()
 
-  // Replace javascript:void(0) paths with your paths
   const navigation = [
-    { title: "Pro version", path: "javascript:void(0)" },
-    { title: "Upgrade", path: "javascript:void(0)" },
-    { title: "Support", path: "javascript:void(0)" },
+    { title: "Versión Pro", path: "/#pricing" },
+    { title: "Soporte", path: "https://linkedin.com/in/kaitakami" },
   ]
 
   const submenuNav = [
-    { title: "Overview", path: "javascript:void(0)" },
-    { title: "Integration", path: "javascript:void(0)" },
-    { title: "Billing", path: "javascript:void(0)" },
-    { title: "Transactions", path: "javascript:void(0)" },
-    { title: "Plans", path: "javascript:void(0)" },
+    { title: "Dashboard", path: "/dashboard" },
+    { title: "Objetivos", path: "/dashboard/objetivos" },
+    { title: "Gastos", path: "/dashboard/gastos" },
+    { title: "Presupuestos", path: "/dashboard/presupuestos" },
   ]
 
 
@@ -77,14 +86,9 @@ const Navbar = () => {
     <header className="text-base lg:text-sm">
       <div className={`bg-white items-center gap-x-14 px-4 max-w-screen-xl mx-auto lg:flex lg:px-8 lg:static ${state ? "h-full fixed inset-x-0" : ""}`}>
         <div className="flex items-center justify-between py-3 lg:py-5 lg:block">
-          <a href="javascript:void(0)">
-            <img
-              src="https://www.floatui.com/logo.svg"
-              width={120}
-              height={50}
-              alt="Float UI logo"
-            />
-          </a>
+          <Link href="/">
+            <Brand imageHeight={25} imageWidth={25} textClassName="text-lg font-bold" />
+          </Link>
           <div className="lg:hidden">
             <button className="text-gray-500 hover:text-gray-800"
               onClick={() => setState(!state)}
@@ -105,23 +109,11 @@ const Navbar = () => {
           </div>
         </div>
         <div className={`nav-menu flex-1 pb-28 mt-8 overflow-y-auto max-h-screen lg:block lg:overflow-visible lg:pb-0 lg:mt-0 ${state ? "" : "hidden"}`}>
-          <ul className="items-center space-y-6 lg:flex lg:space-x-6 lg:space-y-0">
-            <form onSubmit={(e) => e.preventDefault()} className='flex-1 items-center justify-start pb-4 lg:flex lg:pb-0'>
-              <div className="flex items-center gap-1 px-2 border rounded-lg">
-                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="w-full px-2 py-2 text-gray-500 bg-transparent rounded-md outline-none"
-                />
-              </div>
-            </form>
+          <ul className="items-center space-y-6 lg:flex lg:space-x-6 lg:space-y-0  justify-end">
             {
-              navigation.map((item, idx) => {
+              navigation.map((item, id) => {
                 return (
-                  <li key={idx}>
+                  <li key={id}>
                     <a href={item.path} className="block text-gray-700 hover:text-gray-900">
                       {item.title}
                     </a>
@@ -129,17 +121,16 @@ const Navbar = () => {
                 )
               })
             }
-            <AvatarMenue />
+            <AvatarMenu />
           </ul>
         </div>
       </div>
       <nav className="border-b">
         <ul className="flex items-center gap-x-3 max-w-screen-xl mx-auto px-4 overflow-x-auto lg:px-8">
           {
-            submenuNav.map((item, idx) => {
+            submenuNav.map((item, id) => {
               return (
-                // Replace [idx == 0] with [window.location.pathname == item.path]
-                <li key={idx} className={`py-1 ${idx == 0 ? "border-b-2 border-indigo-600" : ""}`}>
+                <li key={id} className={`py-1 ${router.pathname === item.path ? "border-b-2 border-blue-500" : ""}`}>
                   <a href={item.path} className="block py-2 px-3 rounded-lg text-gray-700 hover:text-gray-900 hover:bg-gray-100 duration-150">
                     {item.title}
                   </a>
