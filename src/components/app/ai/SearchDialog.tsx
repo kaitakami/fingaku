c17599995f27d83b99df845f9d6c34c82e991238
Kai Takami <kaitakami@protonmail.com>
2023-05-29 14:05:57 -0600

    refactor: use dialog trigger instead of state

--- src/components/app/ai/SearchDialog.tsx
+++ src/components/app/ai/SearchDialog.tsx
@@ -12,11 +12,12 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
 import { SSE } from 'sse.js'
 import type { CreateCompletionResponse } from 'openai'
-import { X, Loader, User, Frown, CornerDownLeft, Search, Wand } from 'lucide-react'
+import { Loader, User, Frown, CornerDownLeft, Search, Wand } from 'lucide-react'
 
 function promptDataReducer(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -64,7 +65,6 @@ function promptDataReducer(
 }
 
 export function SearchDialog() {
-  const [open, setOpen] = React.useState(false)
   const [search, setSearch] = React.useState<string>('')
   const [question, setQuestion] = React.useState<string>('')
   const [answer, setAnswer] = React.useState<string | undefined>('')
@@ -77,7 +77,6 @@ export function SearchDialog() {
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
       if (e.key === 'k' && e.metaKey) {
-        setOpen(true)
       }
 
       if (e.key === 'Escape') {
@@ -91,7 +90,6 @@ export function SearchDialog() {
   }, [])
 
   function handleModalToggle() {
-    setOpen(!open)
     setSearch('')
     setQuestion('')
     setAnswer(undefined)
@@ -178,121 +176,104 @@ export function SearchDialog() {
   }
 
   return (
-    <>
-      <button
-        onClick={() => setOpen(true)}
-        className="text-base flex gap-2 items-center px-4 py-2 z-50 relative
+    <Dialog>
+      <DialogTrigger className="text-base flex gap-2 items-center px-4 py-2 z-50 relative
         text-slate-500 dark:text-slate-400  hover:text-slate-700 dark:hover:text-slate-300
         transition-colors
         rounded-md
         border border-slate-200 dark:border-slate-500 hover:border-slate-300 dark:hover:border-slate-500
-        min-w-[300px] "
-      >
+        min-w-[300px]">
         <Search width={15} />
         <span className="border border-l h-5"></span>
-        <span className="inline-block ml-4">Search...</span>
-        <kbd
-          className="absolute right-3 top-2.5
-          pointer-events-none inline-flex h-5 select-none items-center gap-1
-          rounded border border-slate-100 bg-slate-100 px-1.5
-          font-mono text-[10px] font-medium
-          text-slate-600 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-400
-          opacity-100 "
-        >
-          <span className="text-xs">⌘</span>K
-        </kbd>{' '}
-      </button>
-      <Dialog open={open}>
-        <DialogContent className="sm:max-w-[850px] text-black">
-          <DialogHeader>
-            <DialogTitle>OpenAI powered doc search</DialogTitle>
-            <DialogDescription>
-              Build your own ChatGPT style search with Next.js, OpenAI & Supabase.
-            </DialogDescription>
-            <hr />
-            <button className="absolute top-0 right-2 p-2" onClick={() => setOpen(false)}>
-              <X className="h-4 w-4 dark:text-gray-100" />
-            </button>
-          </DialogHeader>
+        <span className="inline-block ml-4">Buscar...</span>
+      </DialogTrigger>
+      <DialogContent className="text-black">
+        <DialogHeader>
+          <DialogTitle>FingakuAI</DialogTitle>
+          <DialogDescription>
+            Pregunta sobre fingaku o finanzas.
+          </DialogDescription>
+          <hr />
+        </DialogHeader>
+        <form onSubmit={handleSubmit}>
+          <div className="grid gap-4 py-4 text-slate-700">
+            {question && (
+              <div className="flex gap-4">
+                <span className="bg-slate-100 dark:bg-slate-300 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
+                  <User width={18} />{' '}
+                </span>
+                <p className="mt-0.5 font-semibold text-slate-700 dark:text-slate-100">
+                  {question}
+                </p>
+              </div>
+            )}
 
-          <form onSubmit={handleSubmit}>
-            <div className="grid gap-4 py-4 text-slate-700">
-              {question && (
-                <div className="flex gap-4">
-                  <span className="bg-slate-100 dark:bg-slate-300 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
-                    <User width={18} />{' '}
-                  </span>
-                  <p className="mt-0.5 font-semibold text-slate-700 dark:text-slate-100">
-                    {question}
-                  </p>
-                </div>
-              )}
+            {isLoading && (
+              <div className="animate-spin relative flex w-5 h-5 ml-2">
+                <Loader />
+              </div>
+            )}
 
-              {isLoading && (
-                <div className="animate-spin relative flex w-5 h-5 ml-2">
-                  <Loader />
-                </div>
-              )}
+            {hasError && (
+              <div className="flex items-center gap-4">
+                <span className="bg-red-100 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
+                  <Frown width={18} />
+                </span>
+                <span className="text-slate-700 dark:text-slate-100">
+                  Malas noticias, la busqueda fallo!
+                </span>
+              </div>
+            )}
 
-              {hasError && (
-                <div className="flex items-center gap-4">
-                  <span className="bg-red-100 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
-                    <Frown width={18} />
-                  </span>
-                  <span className="text-slate-700 dark:text-slate-100">
-                    Sad news, the search has failed! Please try again.
-                  </span>
-                </div>
-              )}
-
-              {answer && !hasError ? (
-                <div className="flex items-center gap-4 dark:text-white">
+            {answer && !hasError ? (
+              <div className="flex gap-4 dark:text-white flex-col">
+                <div className='flex items-center gap-2'>
                   <span className="bg-green-500 p-2 w-8 h-8 rounded-full text-center flex items-center justify-center">
                     <Wand width={18} className="text-white" />
                   </span>
-                  <h3 className="font-semibold">Answer:</h3>
-                  {answer}
+                  <h3 className="font-semibold">Respuesta:</h3>
                 </div>
-              ) : null}
-
-              <div className="relative">
-                <Input
-                  placeholder="Ask a question..."
-                  name="search"
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                  className="col-span-3"
-                />
-                <CornerDownLeft
-                  className={`absolute top-3 right-5 h-4 w-4 text-gray-300 transition-opacity ${search ? 'opacity-100' : 'opacity-0'
-                    }`}
-                />
+                <p className='overflow-y-auto'>{answer}</p>
               </div>
-              <div className="text-xs text-gray-500 dark:text-gray-100">
-                Or try:{' '}
-                <button
-                  type="button"
-                  className="px-1.5 py-0.5
+            ) : null}
+
+            <div className="relative">
+              <Input
+                placeholder="Haz una pregunta..."
+                name="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="col-span-3"
+              />
+              <CornerDownLeft
+                className={`absolute top-3 right-5 h-4 w-4 text-gray-300 transition-opacity ${search ? 'opacity-100' : 'opacity-0'
+                  }`}
+              />
+            </div>
+            <div className="text-xs text-gray-500 dark:text-gray-100">
+              O intenta:{' '}
+              <button
+                type="button"
+                className="px-1.5 py-0.5
                   bg-slate-50 dark:bg-gray-500
                   hover:bg-slate-100 dark:hover:bg-gray-600
                   rounded border border-slate-200 dark:border-slate-600
                   transition-colors"
-                  onClick={(_) =>
-                    setSearch('Create a table called profiles with fields id, name, email')
-                  }
-                >
-                  Create a table called profiles with fields id, name, email
-                </button>
-              </div>
+                onClick={() =>
+                  setSearch('Qué es Fingaku?')
+                }
+              >
+                Qué es Fingaku?
+              </button>
             </div>
-            <DialogFooter>
-              <Button type="submit" className="bg-red-500">
-                Ask
-              </Button>
-            </DialogFooter>
-          </form>
-        </DialogContent>
-      </Dialog>
-    </>
+          </div>
+          <DialogFooter>
+            <Button type="submit" className="bg-blue-500">
+              Preguntar
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
   )
 }
