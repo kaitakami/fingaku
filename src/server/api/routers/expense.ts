c525c7fbfb0b7e1e586cca252c59dc9feb365610
Kai Takami <kaitakami@protonmail.com>
2023-05-30 02:41:30 -0600

    feat: create expense router

--- src/server/api/routers/expense.ts
+++ src/server/api/routers/expense.ts
@@ -9,7 +9,6 @@ export const expenseRouter = createTRPCRouter({
         name: z.string(),
         date: z.date(),
         amount: z.number(),
-        userId: z.string(),
         budgetId: z.string().optional(),
       })
     )
@@ -17,11 +16,8 @@ export const expenseRouter = createTRPCRouter({
       try {
         await ctx.prisma.expense.create({
           data: {
-            name: input.name,
-            createdDate: input.date,
-            amount: input.amount,
-            userId: input.userId,
-            budgetId: input.budgetId,
+            ...input,
+            userId: ctx.session.user.id,
           },
         });
       } catch (error) {
@@ -29,5 +25,54 @@ export const expenseRouter = createTRPCRouter({
           "Tuvimos un error en nuestra base de datos, intenta de nuevo"
         );
       }
+    }),
+  ["get-expenses"]: protectedProcedure
+    .input(
+      z.object({
+        startDate: z.number().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const today = new Date();
+      const startDate = new Date();
+      startDate.setDate(today.getDate() - (input.startDate || 10));
+      try {
+        await ctx.prisma.expense.findMany({
+          where: {
+            userId: ctx.session.user.id,
+            createdDate: {
+              gte: startDate,
+              lte: today,
+            },
+          },
+        });
+      } catch (error) {
+        return new ApplicationError(
+          "Tuvimos un error en nuestra base de datos, intenta de nuevo"
+        );
+      }
+    }),
+  ["get-expenses-sum"]: protectedProcedure
+    .input(
+      z.object({ endDate: z.date().optional(), startDate: z.date().optional() })
+    )
+    .query(async ({ ctx, input }) => {
+      const fromDay = input.startDate || new Date();
+      const toDay = input.endDate || new Date();
+      fromDay.setDate(toDay.getDate() - 30);
+
+      return await ctx.prisma.expense.groupBy({
+        by: ["userId"],
+        where: {
+          userId: ctx.session.user.id,
+          createdDate: {
+            gte: fromDay,
+            lte: toDay,
+          },
+        },
+        _sum: {
+          amount: true,
+        },
+      });
     }),
 });
