16fb70bf465272f22bb16973563d08c372d6183e
Kai Takami <kaitakami@protonmail.com>
2023-05-30 02:41:45 -0600

    feat: import all routers to root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,9 @@
 import { createTRPCRouter } from "@/server/api/trpc";
 import { expenseRouter } from "./routers/expense";
 import { incomeRouter } from "./routers/income";
+import { userRouter } from "./routers/user";
+import { transactionRouter } from "./routers/transaction";
+import { goalRouter } from "./routers/goal";
 
 /**
  * This is the primary router for your server.
@@ -8,8 +11,11 @@ import { incomeRouter } from "./routers/income";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
+  userR: userRouter,
   expense: expenseRouter,
   income: incomeRouter,
+  transaction: transactionRouter,
+  goal: goalRouter,
 });
 
 // export type definition of API
