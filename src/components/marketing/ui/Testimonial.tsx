101fea1325f97b3716d37633d61a9ef1362abbfd
Kai Takami <kaitakami@protonmail.com>
2023-05-30 02:36:02 -0600

    chore: remove testimonials avatar

--- src/components/marketing/ui/Testimonial.tsx
+++ src/components/marketing/ui/Testimonial.tsx
@@ -5,37 +5,31 @@ import Image from "next/image"
 
 const testimonials = [
     {
-        avatar: "/testimonial/user6.webp",
         name: "María Rodríguez",
         title: "Estudiante de Economía en la Universidad de Buenos Aires",
         quote: "Fingaku ha sido una herramienta increíble para mi vida financiera como estudiante. Me ha ayudado a establecer metas realistas, realizar un seguimiento de mis gastos y ahorrar de manera efectiva. ¡Recomendaría Fingaku a todos mis compañeros de universidad!"
     },
     {
-        avatar: "/testimonial/user6.webp",
         name: "Diego López",
         title: "Estudiante de Ingeniería en la Universidad Nacional Autónoma de México",
         quote: "Como estudiante, administrar mis finanzas siempre ha sido un desafío. Sin embargo, desde que comencé a usar Fingaku, he sentido un gran alivio. La aplicación es fácil de usar, me permite controlar mis gastos y me motiva a alcanzar mis metas financieras. ¡No puedo imaginar mi vida universitaria sin Fingaku!"
     },
     {
-        avatar: "/testimonial/user6.webp",
         name: "Carolina Fernandez",
         title: "Estudiante de Administración en la Pontificia Universidad Católica de Chile",
         quote: "Fingaku ha sido mi compañero constante en mi viaje financiero como estudiante. Me ha brindado las herramientas necesarias para organizar mis finanzas, establecer metas y tomar decisiones informadas. ¡Gracias a Fingaku, estoy más segura y confiada en mi futuro financiero!"
     },
     {
-        avatar: "/testimonial/user6.webp",
         name: "Andrés Morales",
         title: "Estudiante de Contabilidad en la Universidad de São Paulo",
         quote: "Fingaku ha cambiado completamente la forma en que manejo mis finanzas como estudiante. La aplicación es fácil de usar, me proporciona una visión clara de mi situación financiera y me ayuda a alcanzar mis objetivos. ¡Estoy realmente impresionado y agradecido por esta increíble herramienta!"
     },
     {
-        avatar: "/testimonial/user6.webp",
         name: "Isabela Silva",
         title: "Estudiante de Comunicación en la Universidad de la República de Uruguay",
         quote: "Fingaku ha sido mi salvación en el caos financiero universitario. Me ha permitido establecer presupuestos, ahorrar dinero y tener una mejor comprensión de mis gastos. Es una herramienta imprescindible para cualquier estudiante que desee tener un control financiero sólido y alcanzar sus metas."
     },
     {
-        avatar: "/testimonial/user6.webp",
         name: "Ricardo González",
         title: "Estudiante de Derecho en la Universidad de Buenos Aires",
         quote: "Fingaku ha transformado mi relación con el dinero como estudiante. Me ha ayudado a administrar mis finanzas de manera eficiente, ahorrar dinero y estar más consciente de mis gastos. Es una aplicación esencial para cualquier estudiante que desee tener éxito en su vida financiera."
@@ -52,7 +46,7 @@ const Testimonial = () => {
                         Fingaku es amado por estudiantes alrededor del mundo
                     </h2>
                 </div>
-                <GradientWrapper wrapperClassName="max-w-sm h-40 top-12 inset-x-0" className="mt-12">
+                <GradientWrapper wrapperclassname="max-w-sm h-40 top-12 inset-x-0" className="mt-12">
                     <ul className="grid gap-6 duration-1000 delay-300 ease-in-out sm:grid-cols-2 lg:grid-cols-3">
                         {
                             testimonials.map((item, idx) => (
@@ -68,13 +62,6 @@ const Testimonial = () => {
                                             </p>
                                         </blockquote>
                                         <div className="flex items-center gap-x-4 relative">
-                                            <Image
-                                                src={item.avatar}
-                                                alt={item.name}
-                                                className="rounded-full object-contain"
-                                                width={50}
-                                                height={50}
-                                            />
                                             <div>
                                                 <span className="block text-zinc-800 font-semibold">{item.name}</span>
                                                 <span className="block text-sm mt-0.5">{item.title}</span>
